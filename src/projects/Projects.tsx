a17995a9d5f5848c05b55bf0aaa52114fd150013
chrismelyan <chris.shelpuk@gmail.com>
2022-05-24 23:49:15 +0200

    2: project css

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import s from './Projects.module.css'
-import Project from "./project/Project";
-import stylesContainer from "../common/styles/Container.module.css";
-import Title from "../common/components/title/Title";
+import Project from './project/Project';
+import stylesContainer from '../common/styles/Container.module.css';
+import Title from '../common/components/title/Title';
+import socialImg from '../assets/image/social-network.png';
+import todolistImg from '../assets/image/todolist.png';
 
 const Projects = () => {
+    const social = {
+        backgroundImage: `url(${socialImg})`,
+    };
+    const todolist = {
+        backgroundImage: `url(${todolistImg})`,
+    };
+
     return (
         <div className={s.projectsBlock}>
             <div className={`${s.projectsContainer} ${stylesContainer.container}`}>
                 <Title title={'projects'}/>
                 <div className={s.projects}>
-                    <Project title={'Todolist'} description={'sjdhfbvsdfbkshdbfkcsjdhb'}/>
-                    <Project title={'Social Network'} description={'sjdhfbvsdfbkshdsdfvsdfvbf sdkfjsdf asdkj sdfkj kcsjdhb'}/>
+                    <Project style={todolist} title={'Todolist'} description={'sjdhfbvsdfbkshdbfkcsjdhb'}/>
+                    <Project style={social} title={'Social Network'} description={'sjdhfbvsdfbkshdsdfvsdfvbf sdkfjsdf asdkj sdfkj kcsjdhb'}/>
                 </div>
             </div>
         </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -4,17 +4,19 @@ import s from './Project.module.css'
 type ProjectType = {
     title: string
     description: string
+    style: { backgroundImage: string }
 }
 
 const Project = (props: ProjectType) => {
     return (
         <div className={s.projectBlock}>
-            <div className={s.imgContainer}>
-                <a href={''} className={s.imgLink}>watch</a>
-            </div>
+            <div className={s.imgContainer} style={props.style}/>
             <div className={s.projectDescription}>
                 <h5 className={s.projectTitle}>{props.title}</h5>
                 <span className={s.description}>{props.description}</span>
+                <div>
+                    <a href={''} className={s.imgLink}>Watch</a>
+                </div>
             </div>
         </div>
     );
