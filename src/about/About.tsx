9a07870dba1c6cc0982bcd049e9155692e05fbef
chrismelyan <chris.shelpuk@gmail.com>
2022-08-01 23:21:33 +0200

    3: swiper for skills

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import s from './About.module.scss';
 import stylesContainer from '../common/styles/Container.module.css';
+import photo from '../assets/image/photo.png';
 
 const About = () => {
     return (
         <div className={s.mainBlock}>
             <div className={stylesContainer.container}>
-                <div className={s.photo}>
+                <div className={s.backgroundPicture}>
                     <div className={s.about}>
-                        <h1>Christina Melyan </h1>
+                        <h1>Christina <span>Melyan</span> </h1>
                         <p className={s.text}> a frontend developer</p>
+                    </div>
+                    <div className={s.photo}>
+                        <img src={photo} alt={'avatar'}/>
                     </div>
                 </div>
             </div>
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,22 +3,70 @@ import s from './Skills.module.scss'
 import stylesContainer from '../common/styles/Container.module.css'
 import Skill from "./skill/Skill";
 import Title from "../common/components/title/Title";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {Pagination, Navigation, Mousewheel, Keyboard} from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./skill/styles.css";
 
 const Skills = () => {
     return (
         <div className={s.skillsBlock}>
             <div className={`${s.skillsContainer} ${stylesContainer.container}`}>
                 <Title title={'skills'}/>
-                <div className={s.skills}>
-                    <Skill title={'HTML'} description={'description of HTML and CSS'} icon={'html'}/>
-                    <Skill title={'JavaScript'} description={'description of Javascript'} icon={'javaScript'}/>
-                    <Skill title={'CSS/SASS'} description={'description of CSS and SASS'} icon={'sass'}/>
-                    <Skill title={'React'} description={'description of React'} icon={'react'}/>
-                    <Skill title={'Redux'} description={'description of HTML and CSS'} icon={'redux'}/>
-                    <Skill title={'GitHub'} description={'description of GitHub'} icon={'github'}/>
-                    <Skill title={'Material UI'} description={'description of Material UI'} icon={'material'}/>
-                    <Skill title={'Self study'} description={'Love learning new things'} icon={'learn'}/>
+                <div className={s.paragraph}>
+                    <p>
+                        I have experience in creating SPA using Typescript / React / Redux.
+                        Now working on improving my skills in React and looking for
+                        an entry-level position as a React Front-end Developer.
+                        I am passionate about learning and development. Enjoy working with team members and looking
+                        forward to applying my passion for coding to a talented team to develop quality solutions.
+                    </p>
                 </div>
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={20}
+                    slidesPerGroup={3}
+                    mousewheel={true}
+                    keyboard={true}
+                    loop={true}
+                    loopFillGroupWithBlank={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation, Mousewheel, Keyboard]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <Skill title={'HTML'} description={'description of HTML and CSS'} icon={'html'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Skill title={'JavaScript'} description={'description of Javascript'} icon={'javaScript'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Skill title={'CSS/SASS'} description={'description of CSS and SASS'} icon={'sass'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Skill title={'TypeScript'} description={'description of Typescript'} icon={'typescript'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Skill title={'React'} description={'description of React'} icon={'react'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Skill title={'Redux'} description={'description of HTML and CSS'} icon={'redux'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Skill title={'GitHub'} description={'description of GitHub'} icon={'github'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Skill title={'Material UI'} description={'description of Material UI'} icon={'material'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Skill title={'Self study'} description={'Love learning new things'} icon={'learn'}/>
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </div>
     );
