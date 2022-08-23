8bf3b3679441b4f1f94266b996519a31a7a8e811
chrismelyan <chris.shelpuk@gmail.com>
2022-08-23 22:58:16 +0200

    formik

--- src/contact/contact-form/ContactForm.tsx
+++ src/contact/contact-form/ContactForm.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import s from './ContactForm.module.scss';
-import Button from "../../common/components/button/Button";
+import d from '../../common/components/button/Button.module.scss';
+import {useFormik} from "formik";
 
 const ContactForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: ''
+        },
+        onSubmit: values => {
+            alert(`${values.name} ${values.message}`)
+        }
+    });
     return (
         <div className={s.contactContainer}>
-            <form className={s.formContainer}>
-                <input className={s.textField} placeholder={'Your name'}/>
-                <input className={s.textField} placeholder={'Your email'}/>
-                <textarea className={s.textarea} placeholder={'Your message'}/>
+            <form className={s.formContainer} onSubmit={formik.handleSubmit}>
+                <input
+                    className={s.textField}
+                    id="name"
+                    name="name"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.name}
+                    placeholder={'Your name'}
+                />
+                <input
+                    className={s.textField}
+                    placeholder={'Your email'}
+                    id="email"
+                    name="email"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+                <textarea
+                    className={s.textarea}
+                    placeholder={'Your message'}
+                    id="message"
+                    name="message"
+                    onChange={formik.handleChange}
+                    value={formik.values.message}
+                />
+
+                <div className={s.buttonContainer}>
+                    <button type={'submit'} className={`${s.button} ${d.button}`}>Send</button>
+                </div>
+
             </form>
-            <div className={s.buttonContainer}>
-                <Button style={s.button} title={'send'} callback={() => {}}/>
-            </div>
         </div>
     );
 };
